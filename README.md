4dfac974fefddd5281833a8e52e68ba211a3c32c
hudadamar21 <hudadamar21@gmail.com>
2023-06-26 22:02:38 +0700

    init flutter

---
+++ README.md
@@ -0,0 +1,3 @@
+# dirbbird
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/dirbbird/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dirbbird
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
