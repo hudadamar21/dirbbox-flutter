5e4f49e603c0e2884faaed4d807e5496d1f85f9b
hudadamar21 <hudadamar21@gmail.com>
2023-07-15 22:28:05 +0700

    complete app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
+import 'package:dirbbird/provider/drawer_provider.dart';
+import 'package:dirbbird/screens/change_password_screen.dart';
+import 'package:dirbbird/screens/profile_screen.dart';
+import 'package:dirbbird/screens/settings_screen.dart';
+import 'package:dirbbird/screens/storage_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'package:dirbbird/screens/login_screen.dart';
+import 'package:dirbbird/screens/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => DrawerProvider()),
+        ],
+        child: const MainApp(),
+      ),
+    );
+  });
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +31,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/home': (context) => HomeScreen(),
+        '/profile': (context) => ProfileScreen(),
+        '/storage': (context) => StorageScreen(),
+        '/settings': (context) => SettingsScreen(),
+        '/settings/change-password': (context) => ChangePasswordScreen(),
+      },
+      theme: ThemeData(
+        textTheme: GoogleFonts.openSansTextTheme(
+          Theme.of(context).textTheme,
         ),
       ),
+      home: const LoginScreen(),
     );
   }
 }
