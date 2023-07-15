abe64d07e32a6ff77c516607017dd34e50557b45
hudadamar21 <hudadamar21@gmail.com>
2023-07-15 22:22:57 +0700

    Create STORAGE_COLORS.dart

---
+++ lib/consts/STORAGE_COLORS.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class StorageColors {
+  static Color images = const Color.fromARGB(255, 255, 198, 74);
+  static Color videos = const Color.fromARGB(255, 157, 233, 70);
+  static Color documents = const Color.fromARGB(255, 90, 119, 248);
+  static Color designFiles = const Color.fromARGB(255, 2, 26, 63);
+  static Color others = const Color.fromARGB(255, 247, 158, 24);
+}
